5ee19ebc373a4399ecd54d4cc6edd4d740a54fed
xuhaoyang <497633959@qq.com>
2017-03-02 23:26:33 +0800

    fix problem

--- app/src/main/java/com/xhy/weibo/api/NetParams.java
+++ app/src/main/java/com/xhy/weibo/api/NetParams.java
@@ -7,7 +7,7 @@ package com.xhy.weibo.api;
 public class NetParams {
 
     public static String uploadUserPic(String token, int height, int width) {
-        String result = com.xhy.weibo.network.URLs.WEIBO_UPLOAD_PIC + "?token=" + token + "&height=" + height
+        String result = URLs.WEIBO_UPLOAD_PIC + "?token=" + token + "&height=" + height
                 + "&width=" + width;
         return result;
     }
